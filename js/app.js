8fe7f8030958d90ae270bf5736553451ccbef0d9
omimoustakim <omimoustakim@gmail.com>
2026-09-08 18:39:18 +0000

    Suppression image grillz-36.webp

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
     "grillz-21.webp", "grillz-22.webp", "grillz-23.webp", "grillz-24.webp",
     "grillz-25.webp", "grillz-26.webp", "grillz-27.webp", "grillz-28.webp",
     "grillz-29.webp", "grillz-30.webp", "grillz-31.webp", "grillz-32.webp",
-    "grillz-33.webp", "grillz-34.webp", "grillz-35.webp", "grillz-36.webp",
+    "grillz-33.webp", "grillz-34.webp", "grillz-35.webp", "grillz-37.webp",
     "grillz-37.webp", "grillz-38.webp"
   ];
   var galleryTypes = ["Gold", "Custom", "Chrome-Cobalt", "Iced Out"];
